3398a4b40789089b0c795231987c627fd0c75c71
abdussamettrkr <abdussamettrkr@gmail.com>
2024-07-14 15:19:58 +0300

    Add shard and replicate

--- distributed.py
+++ distributed.py
@@ -5,6 +5,7 @@ from typing import Any, Sequence
 
 import torch
 import torch.distributed as dist
+from torch.distributed._tensor import distribute_tensor, Shard, Replicate, DTensor, init_device_mesh, DeviceMesh
 
 import utils
 
@@ -18,7 +19,9 @@ class DistributionCenter:
     # The Skynet. It handles communication between proceses and sends instructions.
     def __init__(self, nproc: int) -> None:
         self.nproc = nproc
+
         self.tensor_counter = 0 #??
+        self.tensor_ref: dict[int, int] = {}
 
         ctx = mp.get_context("spawn")
 
@@ -27,7 +30,7 @@ class DistributionCenter:
         for process in processes:
             process.start()
         
-        utils.init_distributed(rank=0, world_size=self.nproc, device="cpu")
+        self._initilize_distributed(rank=0, world_size=self.nproc)
         self.processes = processes
 
 
@@ -35,23 +38,42 @@ class DistributionCenter:
         # Broadcast
         self._send_instrcs(Instructions.BROADCAST, list(tensor.shape))
         dist.broadcast(tensor, src = 0)
+        self.tensor_ref[id(tensor)] = self.tensor_counter
 
         # Shrad
-        pass
-    
+        self._send_instrcs(Instructions.SHARD, self.tensor_counter)
+        dtensor = distribute_tensor(tensor, self.device_mesh, [Shard(0)])
+        self.tensor_ref[id(dtensor)] = self.tensor_counter+1
+        self.tensor_counter += 2
+        return dtensor
+
+    def replicateTensor(self, tensor: torch.Tensor):
+        # Broadcast
+        self._send_instrcs(Instructions.BROADCAST, list(tensor.shape))
+        dist.broadcast(tensor, src = 0)
+        self.tensor_ref[id(tensor), self.tensor_counter]
+
+        # Replicate
+        self._send_instrcs(Instructions.REPLICATE, self.tensor_counter, [Replicate()])
+        dtensor = distribute_tensor(tensor, mesh = self.device_mesh)
+        self.tensor_ref[id(dtensor), self.tensor_counter+1]
+        self.tensor_counter += 2
+        return dtensor
+
+
 
     def _send_instrcs(self, instruction: Instructions, arg1: Any):
         dist.broadcast_object_list((instruction, arg1))
-        pass
-    
 
 
+    def _initilize_distributed(self, rank:int, world_size:int):
+        utils.init_distributed(rank=rank, world_size=self.nproc, device="cpu")
+        self.device_mesh = init_device_mesh("cpu", (world_size,))
+        
 
     def process(self, rank: int):
         # Initialize backend
-        utils.init_distributed(rank=rank, world_size=self.nproc, device="cpu")
-
-
+        self._initilize_distributed(rank, self.nproc)
 
         while True:
             instruction = [None, None]
@@ -63,10 +85,17 @@ class DistributionCenter:
                     assert isinstance(arg, Sequence)
                     tensor = torch.empty(arg)
                     dist.broadcast(tensor, src = 0)
-                    print(f"This is rank: {os.environ['RANK']} and my tensor is:", tensor)
+                    self.tensor_ref[self.tensor_counter] = tensor
+                    self.tensor_counter += 1
 
                 case Instructions.SHARD:
-                    pass
+                    tensor = self.tensor_ref[arg]
+                    dtensor = distribute_tensor(tensor, self.device_mesh, [Shard(0)])
+                    self.tensor_ref[self.tensor_counter] = dtensor
+                    self.tensor_counter += 1
+
                 case Instructions.REPLICATE:
-                    pass
-            
+                    tensor = self.tensor_ref[arg]
+                    dtensor = distribute_tensor(tensor, self.device_mesh, [Replicate()])
+                    self.tensor_ref[self.tensor_counter] = dtensor
+                    self.tensor_counter += 1
